f5ca0c8bbee99862cdfd0495da6bacaa36a3ba0e
Seryang Choe <seryang@users.noreply.github.com>
2017-01-06 10:22:49 +0900

    Update RankApplicationTests.java

--- src/test/java/com/RankApplicationTests.java
+++ src/test/java/com/RankApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SmilegateTestApplicationTests {
+public class RankApplicationTests {
 
 	@Test
 	public void contextLoads() {
