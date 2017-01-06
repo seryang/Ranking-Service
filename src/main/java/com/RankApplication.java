7898f156831b53432fb1b04c021e832dc081f2d1
Seryang Choe <seryang@users.noreply.github.com>
2017-01-06 10:23:33 +0900

    update & rename

--- src/main/java/com/RankApplication.java
+++ src/main/java/com/RankApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SmilegateTestApplication {
+public class RankApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(SmilegateTestApplication.class, args);
+		SpringApplication.run(RankApplication.class, args);
 		System.out.println("server on");
 	}
 }
